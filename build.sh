94ef9c979c4c1b2cec36899815b6f3d25a1886ba
Stavros Niafas <sniafas@tuta.io>
2021-10-21 01:45:56 +0300

    [chore]: update project build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Make project structure"
-mkdir -p trained_models/{baseline,single,loop,logs}
+mkdir -p trained_models/{baseline,randomly_results,single,loop,logs}
 mkdir -p trained_models/baseline/{densenet,vgg}
+mkdir -p trained_models/randomly_results/{densenet,vgg}
 mkdir -p trained_models/single/{densenet,vgg}
 mkdir -p trained_models/loop/{densenet,vgg}
