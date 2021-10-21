6f9555cfd079f67d77adcfdd70364edf20644949
Stavros Niafas <sniafas@gmail.com>
2021-10-22 00:12:27 +0300

    [fix]: create dataset dir

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Make project structure"
+mkdir -p dataset/{csv,images}
 mkdir -p trained_models/{baseline,randomly_results,single,loop,logs}
 mkdir -p trained_models/baseline/{densenet,vgg}
 mkdir -p trained_models/randomly_results/{densenet,vgg}
