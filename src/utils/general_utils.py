d7c9f4911da14dd1ecc75386a433974ef9e57597
Stavros Niafas <sniafas@gmail.com>
2021-10-23 16:52:22 +0300

    [feat]: get training artifact name paths

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -27,3 +27,43 @@ def get_filepath(path_from_module: str, file_name: str) -> str:
 
     fn = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))).split("src")[0]
     return "{0}{1}/{2}".format(fn, path_from_module, file_name)
+
+
+def get_artifact_names(base_dir: str, model_arch: str, cardinality_split: int, seed: int):
+    """Create artifact save names
+
+    Args:
+        base_dir (str): base dir run name
+        model_arch (str): model arch
+        cardinality_split (int): number of samples appended
+        seed (int): seed
+    """
+
+    save_model_name = get_filepath(
+        f"trained_models/{base_dir}/{model_arch}", f"model_{cardinality_split}_seed_{seed}.h5"
+    )
+    save_training_hist_name = get_filepath(
+        f"trained_models/{base_dir}/{model_arch}",
+        f"training_hist_model_{cardinality_split}_seed_{seed}",
+    )
+    save_training_hist_plot = get_filepath(
+        f"trained_models/{base_dir}/{model_arch}",
+        f"learning_history_{cardinality_split}_seed_{seed}",
+    )
+    save_test_results_name = get_filepath(
+        f"trained_models/{base_dir}/{model_arch}",
+        f"test_results_model_{cardinality_split}_seed_{seed}.json",
+    )
+    save_cm_name = get_filepath(f"trained_models/{base_dir}/{model_arch}", f"cm_model_{cardinality_split}_{seed}")
+    save_report_name = get_filepath(
+        f"trained_models/{base_dir}/{model_arch}", f"report_model_{cardinality_split}_seed_{seed}"
+    )
+
+    return (
+        save_model_name,
+        save_training_hist_name,
+        save_training_hist_plot,
+        save_test_results_name,
+        save_cm_name,
+        save_report_name,
+    )
