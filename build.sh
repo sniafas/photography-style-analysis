58ca863ddfa4f91daa2354a837211ae62c8aa163
Stavros Niafas <sniafas@gmail.com>
2021-10-23 16:54:30 +0300

    [fix]: project's structure

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 echo "Make project structure"
 mkdir -p dataset/{csv,images}
-mkdir -p trained_models/{baseline,randomly_results,single,loop,logs}
-mkdir -p trained_models/baseline/{densenet,vgg}
+mkdir -p dataset/csv/active_learning
+mkdir -p dataset/csv/active_learning/{all,all_ceal}
+mkdir -p trained_models/{baseline,randomly_results,actively_results_sqal,actively_results_sqal_ceal,actively_results_all,actively_results_all_ceal,logs}
 mkdir -p trained_models/randomly_results/{densenet,vgg}
-mkdir -p trained_models/single/{densenet,vgg}
-mkdir -p trained_models/loop/{densenet,vgg}
+mkdir -p trained_models/actively_results_sqal/{densenet,vgg}
+mkdir -p trained_models/actively_results_sqal_ceal/{densenet,vgg}
+mkdir -p trained_models/actively_results_all/{densenet,vgg}
+mkdir -p trained_models/actively_results_all_ceal/{densenet,vgg}
